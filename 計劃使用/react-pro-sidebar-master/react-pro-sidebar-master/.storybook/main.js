61cfb8fdbec208114901e53ba46161cb1eed6ec6
小森森 <ray0814@gmail.com>
2025-09-11 15:36:51 +0800

    mod files.

--- 計劃使用/react-pro-sidebar-master/react-pro-sidebar-master/.storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../storybook/**/*.stories.@(tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
-};
--- 計劃使用/react-pro-sidebar-master/react-pro-sidebar-master/.storybook/preview.js
+++
@@ -1,7 +0,0 @@
-export const parameters = {
-  options: {
-    storySort: {
-      order: ['Sidebar', 'Menu', 'MenuItem', 'SubMenu', 'Playground'],
-    },
-  },
-};
